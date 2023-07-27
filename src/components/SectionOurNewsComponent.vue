a380bc26bf59448ae5c85aecc69bbb1b33afb9d8
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-07-27 14:16:13 +0200

    carousel

--- src/components/SectionOurNewsComponent.vue
+++ src/components/SectionOurNewsComponent.vue
@@ -55,7 +55,7 @@ export default{
 </script>
 
 <template>
-    <section class="p-5">
+    <section class="section-one p-5">
         <!-- Title Section Main Component Our Work -->
         <div class="text-center text-black p-2">
             <h4 class="fw-bold">Our News</h4>
@@ -79,34 +79,36 @@ export default{
         </div>
     </section>
 
-    <section>
+    <section class="section-two container">
+        <div class="text-center fs-4 fw-bold m-5 text-black">What Our Client Say</div>
         <div id="carouselExampleDark" class="carousel carousel-dark slide">
             <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active button-circle" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" class="button-circle" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" class="button-circle" aria-label="Slide 3"></button>
+
             </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="..." class="d-block w-100" alt="...">
+            <div class="carousel-inner height">
+                <div class="carousel-item active profile-pic  d-flex justify-content-center">
+                    <img src="../assets/img/testimonials-1.jpg" class="d-block w-100" alt="testimonials">
+                </div>
                 <div class="carousel-caption d-none d-md-block">
                     <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
+                    <p>Luis De Salvo CREO TECH</p>
                 </div>
-            </div>
-            <div class="carousel-item" data-bs-interval="2000">
-                    <img src="..." class="d-block w-100" alt="...">
+                <div class="carousel-item profile-pic  d-flex justify-content-center">
+                    <img src="../assets/img/testimonials-2.jpg" class="d-block w-100" alt="testimonials">
+                </div>
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Second slide label</h5>
                     <p>Some representative placeholder content for the second slide.</p>
                 </div>
-            </div>
-                <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
+                <div class="carousel-item height profile-pic d-flex justify-content-center">
+                    <img src="../assets/img/testimonials-1.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-caption d-none d-md-block">
                         <h5>Third slide label</h5>
                         <p>Some representative placeholder content for the third slide.</p>
-                    </div>
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
@@ -118,8 +120,16 @@ export default{
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
+        <hr class="container">
+        <div class="container d-flex justify-content-between container-logo-partners">
+            <img src="../assets/img/clients_partner_5-200x202.png" alt="client-partners">
+            <img src="../assets/img/clients_partner_6-200x202.png" alt="client-partners">
+            <img src="../assets/img/clients_partner_1-200x202.png" alt="client-partners">
+            <img src="../assets/img/clients_partner_2-200x202.png" alt="client-partners">
+        </div>
+        
     </section>
-    
+
 </template>
 
 <style lang="scss" scoped>
@@ -128,7 +138,8 @@ export default{
 @use "../assets/scss/main.scss" as *;
 @use "../assets/scss/partials/variables.scss" as*;
 
-section{
+// prima section
+.section-one{
     background-color: $second-color-bg;
     height: 800px;
 
@@ -150,8 +161,40 @@ section{
         align-items:center; 
         padding:15px;
     }
+}
+//section two
+.section-two{
 
-
+    height: 400px;
+    .profile-pic{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #0CD977;
+    height: 100px;
+    width: 100px;
+    overflow: hidden;
+    border-radius: 50%;
 }
 
+.container-logo-partners{
+    height: 200px;
+    width: 1000px;
+    padding: 50px;
+  }
+
+.height{
+    height: 250px;
+    display: flex;
+    justify-content: center;
+    padding-right: 70px;
+}
+
+.button-circle{
+    border-radius: 50%;
+    height: 10px;
+    width: 10px;
+}
+
+}
 </style>
