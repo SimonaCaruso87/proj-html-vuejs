4300d619537c1d4535e0bb806e39cad5d70c8dfe
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-07-27 18:18:49 +0200

    Esercizio completato

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -6,10 +6,35 @@ export default{
     name: "FooterComponent",
     data(){
         return{
-         store
+         store ,
+         linkNavbar:[
+            { 
+                text: "Home",
+            },
+            {
+                text: "Who we are" ,
+            },
+            {
+                text: "What we do" , 
+            },
+            {
+                text: "Where we work",
+            },
+            {
+                text: "Careers",
+            },
+            {
+                text: "News",
+            },
+            {
+                text: "Get In Touch Now",
+            },
+            
+          ],
         }
     },
     methods:{
+
 
     }
 }
@@ -18,7 +43,18 @@ export default{
 <template>
 
     <footer>
-        FOOTER
+        <div>
+            <img src="../assets/img/logo_seo_1x.png" alt="logo">
+        </div>
+        <div class=" container d-flex justify-content-center align-items-center nav-b-footer">
+            <ul v-for="(link , i ) in linkNavbar" :key="i" class="navbar-nav menu d-inline-block text-secondary-emphasis">
+    <!-- Array element menu -->        
+                    <li class="nav-item">
+                        <a class="nav-link active font-size p-4" aria-current="page" href="#">{{ link.text }}</a>
+                    </li>  
+            </ul>
+        </div>
+
     </footer>
 
 </template>
@@ -28,6 +64,19 @@ export default{
 footer{
     padding:40px;
     text-align: center;
+
+    img{
+        width: 100px;
+        object-fit: cover;
+    }
+
+    .font-size{
+        font-size: 12px;
+    }
+
+    nav-b-footer:hover{
+        background-color:#071B50;
+    }
 }
 
 </style>
