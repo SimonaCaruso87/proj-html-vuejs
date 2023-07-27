42047efbbd260805f7144bd2229c243bbb19d1da
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-07-27 14:27:02 +0200

    Section consulation

---
+++ src/components/SectionConsultation.vue
@@ -0,0 +1,68 @@
+<script>
+
+import { store } from '../store.js';
+
+
+export default{
+    name: "SectionConsultation",
+    data(){
+        return{ 
+          store ,
+        }
+    },
+    methods:{
+       
+    }
+}
+</script>
+
+<template>
+    <section>
+        <!-- Title Section Main Component Our Work -->
+        <div class="text-center text-black pt-2">
+            <h4 class="fw-bold">Start Your Free Consultation</h4>
+            <h6 class="font-size">Sed ud perspicitis dolor sit amet consectetur adipisicing elit. In accusamus</h6>
+            <h6 class="font-size">tempore laboriosam minus!</h6>
+        </div>
+        <!-- Container Card -->
+        <div class="container">
+        <!-- Numero 6 Card in array -->
+        </div>   
+    </section>
+</template>
+
+<style lang="scss" scoped>
+
+@use "../assets/scss/partials/mixins" as *;
+@use "../assets/scss/main.scss" as *;
+@use "../assets/scss/partials/variables.scss" as*;
+
+section{
+    height: 300px;
+    background-image: url(../assets/img/case-study-gallery-3-1-800x600.jpg);
+    background-size: contain;
+    background-repeat: no-repeat;
+    opacity: rgba(0, 0, 0, 0.527);
+    
+
+    // .font-size{
+    //    font-size: 12px;
+    // }
+
+    // .font-section{
+    //     color:orange; 
+    //     display: block; 
+    //     height: 50px;
+    // }
+    
+    // .size{
+    //     text-align: center; 
+    //     display:flex; 
+    //     flex-wrap: wrap; 
+    //     justify-content:center; 
+    //     align-items:center; 
+    //     padding:15px;
+    // }
+}
+
+</style>
